ac338e85b6cea133bd9eb143d2edd6fc93791f9d
老蔡 <40415157+chunchengmeigui@users.noreply.github.com>
2019-03-17 01:14:52 +0800

    laui文件上传下载

---
+++ layui-up-down-file/src/main/java/com/oceam/mapper/TPowerMapper.java
@@ -0,0 +1,25 @@
+package com.oceam.mapper;
+
+import com.oceam.entity.TPower;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
+public interface TPowerMapper {
+    int deleteByPrimaryKey(String roleId);
+
+    int insert(TPower record);
+
+    int insertSelective(TPower record);
+
+    TPower selectByPrimaryKey(String roleId);
+
+    int updateByPrimaryKeySelective(TPower record);
+
+    int updateByPrimaryKeyWithBLOBs(TPower record);
+
+    int updateByPrimaryKey(TPower record);
+
+     List<TPower> selectAll(@Param(value = "roleName") String roleName);
+}
