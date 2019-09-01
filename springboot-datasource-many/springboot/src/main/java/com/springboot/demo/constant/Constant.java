f735a473b3c6095d9eab94f4e2ec25cdad3a5d08
chunchengmeigui <903321643@qq.com>
2019-09-01 15:19:54 +0800

    springboot整合多数据源及事务配置

---
+++ springboot-datasource-many/springboot/src/main/java/com/springboot/demo/constant/Constant.java
@@ -0,0 +1,45 @@
+package com.springboot.demo.constant;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ *@Date 2019-09-01 01:20:27
+ */
+@SuppressWarnings("ALL")
+public class Constant {
+    /**
+     * 暂无数据
+     */
+    public static final int CODE_EMPTY = 1001;
+    public static final String CODE_EMPTY_MSG = "暂无数据";
+
+    /**
+     * OK
+     */
+    public static final int CODE_OK = HttpStatus.OK.value();
+    public static final String CODE_OK_MSG = "ok";
+
+    /**
+     * 操作失败
+     */
+    public static final int CODE_DEFALT = 400;
+    public static final String CODE_DEFALT_MSG = "操作失败";
+
+    /**
+     * 参数错误
+     */
+    public static final int CODE_CONDITION_ERROR = 1002;
+    public static final String CODE_CONDITION_ERROR_MSG = "参数错误";
+
+    /**
+     * 系统异常
+     */
+    public static final int CODE_SYSTEM_ERROR = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    public static final String CODE_SYSTEM_ERROR_MSG = "系统异常";
+
+    /**
+     * 参数错误
+     */
+    public static final int CODE_CONDITION_USE = 1100;
+    public static final String CODE_CONDITION_USE_MSG = "存在相同信息记录";
+}
