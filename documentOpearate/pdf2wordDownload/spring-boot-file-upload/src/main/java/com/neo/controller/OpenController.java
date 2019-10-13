47e25bd35c1eec903fa756cb5525b75bdb0a9a21
903321643@aa.com <d903321643>
2019-10-13 23:08:31 +0800

    pdf2wordDownload

---
+++ documentOpearate/pdf2wordDownload/spring-boot-file-upload/src/main/java/com/neo/controller/OpenController.java
@@ -0,0 +1,29 @@
+package com.neo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 功能简述：
+ *
+ * @author caidingnu
+ * @create 2019-04-27 00:33
+ * @since 1.0.0
+ */
+@Controller
+public class OpenController {
+
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+    @GetMapping("/one")
+    public String one() {
+        return "editOnePicUp";
+    }
+    @GetMapping("/many")
+    public String many() {
+        return "editManyPicUp";
+    }
+
+}
---
+++ documentOpearate/pdf2wordDownload/spring-boot-file-upload/src/main/resources/static/js/system.js
@@ -0,0 +1,1 @@
+UPLOAD_FILE_Path="http://192.168.1.2:8080/photo/";
