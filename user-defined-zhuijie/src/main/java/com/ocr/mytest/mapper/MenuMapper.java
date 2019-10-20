1f9fa4443fcf2b41ea80710b569b47fad5eb20e9
903321643@aa.com <d903321643>
2019-10-21 01:31:15 +0800

    大点的

---
+++ user-defined-zhuijie/src/main/java/com/ocr/mytest/mapper/MenuMapper.java
@@ -0,0 +1,22 @@
+package com.ocr.mytest.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.mybatis.spring.annotation.MapperScan;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * desc:
+ * author caidingnu
+ * create 2019/10/21
+ * version 1.0.0
+ */
+@Mapper
+@MapperScan("")
+public interface MenuMapper {
+
+    @Select("select * from menu")
+    List<Map<String,Object>> selectAll();
+}
