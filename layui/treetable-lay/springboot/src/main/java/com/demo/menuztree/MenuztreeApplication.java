c7e2c298cec83b211b637234c3431692b34e0b45
903321643@aa.com <d903321643>
2019-11-16 02:33:27 +0800

    的

---
+++ layui/treetable-lay/springboot/src/main/java/com/demo/menuztree/MenuztreeApplication.java
@@ -0,0 +1,14 @@
+package com.demo.menuztree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuztreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuztreeApplication.class, args);
+        System.out.println("========测试树启动成功==============");
+    }
+
+}
---
+++ layui/treetable-lay/springboot/src/main/java/com/demo/menuztree/mapper/MenuMapper.java
@@ -0,0 +1,30 @@
+package com.demo.menuztree.mapper;
+
+import com.demo.menuztree.entity.Menu;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface MenuMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Menu record);
+
+    int insertSelective(Menu record);
+
+    Menu selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Menu record);
+
+    int updateByPrimaryKey(Menu record);
+
+    List<Menu> selectAll();
+
+
+    List<Menu> all();
+
+    List<Menu> selectOne(Integer pid);
+
+}
