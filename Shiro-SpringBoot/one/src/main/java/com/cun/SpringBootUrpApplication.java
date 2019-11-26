11f979cdb99f3e4238f347aa5f707efd8e7630af
903321643@aa.com <d903321643>
2019-11-26 21:36:04 +0800

    shiro

---
+++ Shiro-SpringBoot/one/src/main/java/com/cun/SpringBootUrpApplication.java
@@ -0,0 +1,22 @@
+package com.cun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@SpringBootApplication
+public class SpringBootUrpApplication extends WebMvcConfigurationSupport {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUrpApplication.class, args);
+    }
+
+    //这里配置静态资源文件的路径导包都是默认的直接导入就可以
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+        super.addResourceHandlers(registry);
+    }
+}
---
+++ Shiro-SpringBoot/one/src/main/java/com/cun/exception/NoAuthorityException.java
@@ -0,0 +1,31 @@
+package com.cun.exception;
+
+import org.apache.shiro.authz.AuthorizationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * desc：
+ * author CDN
+ * create 2019-09-23 20:01
+ * version 1.0.0
+ */
+@RestControllerAdvice
+public class NoAuthorityException {
+
+
+    @ExceptionHandler(value = AuthorizationException.class)//处理访问方法时权限不足问题
+    public Map<String, Object> defaultErrorHandler(HttpServletRequest req, Exception e) {
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("success", false);
+        map.put("msg", "无权限！");
+
+        return map;
+    }
+
+}
