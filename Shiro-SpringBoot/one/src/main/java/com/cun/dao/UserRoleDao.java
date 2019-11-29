f5e8a2ac31d50cfcc2a4706400f11d72bae697d4
903321643@aa.com <d903321643>
2019-11-30 00:08:36 +0800

    三三数

--- Shiro-SpringBoot/one/src/main/java/com/cun/dao/UserRoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.cun.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cun.entity.UserRole;
-
-public interface UserRoleDao extends JpaRepository<UserRole, Integer> {
-
-}
