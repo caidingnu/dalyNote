6f5e7244dca48557e7c6b531860ef90e3725e506
903321643@aa.com <d903321643>
2019-12-27 22:57:47 +0800

    plus 分页

---
+++ mybatisPlus/mybatisPlus四种分页查询/mybatisplusPage/src/main/java/com/example/mybatisplustest/mapper/UniDataMapper.java
@@ -0,0 +1,49 @@
+package com.example.mybatisplustest.mapper;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mybatisplustest.entity.UniData;
+import com.example.mybatisplustest.mapper.provider.UniDataProvider;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+
+import java.util.Map;
+
+/**
+ * desc:
+ * author caidingnu
+ * create 2019/12/27
+ * version 1.0.0
+ */
+public interface UniDataMapper {
+
+    /**
+     * 注解方式分页
+     *
+     * @param page
+     * @param map
+     * @return
+     */
+    @Select("SELECT user.name,user.password,USER.nick_name as nickName,USER.age,role.role_id as roleId,role.role " +
+            "FROM user INNER JOIN role ON `user`.user_id = role.role_id where user.user_id = #{map.userId}")
+    IPage<UniData> annotationsPage(IPage<UniData> page, @Param("map") Map map);
+
+    /**
+     * provider 方式分页
+     *
+     * @param page
+     * @param map
+     * @return
+     */
+    @SelectProvider(type = UniDataProvider.class, method = "providerPage")
+    IPage<UniData> providerPage(IPage<UniData> page, @Param("map") Map map);
+
+    /**
+     *  xml 方式分页
+     * @param page
+     * @param map
+     * @return
+     */
+    IPage<UniData> xmlPage(IPage<UniData> page, @Param("map") Map map);
+}
