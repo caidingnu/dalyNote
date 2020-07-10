4a04940c35f02b1fe1f8924e8968ffd09f59c9bd
cdn <903321643@qq.com>
2020-07-10 13:35:55 +0800

    循环配置文件自动注册

---
+++ springboot/springboot-multiple-datasource/dynamic-datasource-aop-loop/src/main/java/com/example/demo/entity/ClassEnt.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+
+public class ClassEnt implements Serializable {
+    private Integer classId;
+
+    private Integer userId;
+
+    private String className;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getClassId() {
+        return classId;
+    }
+
+    public void setClassId(Integer classId) {
+        this.classId = classId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className == null ? null : className.trim();
+    }
+}
---
+++ springboot/springboot-multiple-datasource/dynamic-datasource-aop-loop/src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,87 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private String code;
+
+    private String className;
+
+    private Integer classId;
+
+    private String mobile;
+
+    private Integer gender;
+
+    private String remark;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className == null ? null : className.trim();
+    }
+
+    public Integer getClassId() {
+        return classId;
+    }
+
+    public void setClassId(Integer classId) {
+        this.classId = classId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+}
